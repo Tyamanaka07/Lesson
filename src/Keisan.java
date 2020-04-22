8956a75cfa4ed64f28000e92851fcced27de3780
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-04-22 15:08:17 +0900

    入力した数値を足し算(正しいほう)

---
+++ src/Keisan.java
@@ -0,0 +1,15 @@
+
+public class Keisan {
+
+	public static void main(String[] args) {
+		System.out.print("1つ目の数値を入力してください ：");
+		int input1 = new java.util.Scanner(System.in).nextInt();
+
+		System.out.print("2つ目の数値を入力してください ：");
+		int input2 = new java.util.Scanner(System.in).nextInt();
+
+		System.out.println(input1 + input2);
+
+	}
+
+}
