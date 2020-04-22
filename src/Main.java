3c681e893c49e1c2eb8220817a9dfe4f09552f11
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-04-22 13:28:19 +0900

    変えました。

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+//https://github.com/Tyamanaka07/Lesson.git
 
 public class Main {
 
 	public static void main(String[] args) {
+		int tuki = 4;
+		int hi = 22;
 		double p = 3.14;
 
+		System.out.println("本日は" + tuki + "月" + hi + "日です");
 		System.out.println("円周率は" + p + "です");
 	}
 }
