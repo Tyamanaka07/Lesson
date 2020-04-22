03d1b079988d4194893955a2d1a36c29c0a5df9c
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-04-22 14:16:58 +0900

    平均算出

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
-//https://github.com/Tyamanaka07/Lesson.git
-
 public class Main {
-
 	public static void main(String[] args) {
-		int tuki = 4;
-		int hi = 22;
-		double p = 3.14;
+		int a;
+		int b;
+		a = 20;
+		b = a + 5;
+		int num = 10;
+		int amari = num%3;
 
-		System.out.println("本日は" + tuki + "月" + hi + "日です");
-		System.out.println("円周率は" + p + "です");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("今日は\"四月\"です");
+		System.out.println("田尻さんの本日の支払いは\"\\1000000\"だなも");
+		System.out.println("C:\\に\"フォルダ\"を作成");
+
+		System.out.println(amari);
+
+		num += 5;
+		num++;
+		System.out.println(num);
+
+		/*double d = 3;
+		int n = (int)3.0;
+		String s = "ベスト" + 3;*/
+
+		int kokugo = 70;
+		int sansu = 75;
+
+		System.out.println((double)(kokugo + sansu) / 2);
 	}
 }
