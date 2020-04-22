e89bd88082dc9012a25d20ea0415b5ffc5df532d
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-04-22 16:31:59 +0900

    九九の問題

--- src/kuku.java
+++ src/kuku.java
@@ -4,14 +4,14 @@ public class kuku {
 	public static void main(String[] args) {
 		System.out.println("九九の問題!!");
 
-		int suti1 = new java.util.Random().nextInt(8);
-		int suti2 = new java.util.Random().nextInt(8);
+		int suti1 = new java.util.Random().nextInt(9) + 1;
+		int suti2 = new java.util.Random().nextInt(9) + 1;
 
 		System.out.print(suti1 + "*" + suti2 + "=" );
-
-		int you_answer = new java.util.Scanner(System.in).nextInt();
 		int answer = suti1 * suti2;
 
+		int you_answer = new java.util.Scanner(System.in).nextInt();
+		
 		if(answer == you_answer) {
 			System.out.println("正解!!");
 		}
