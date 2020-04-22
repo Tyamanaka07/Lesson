bb870427331ef5617120e8c5e91e3135e791f622
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-04-22 15:06:58 +0900

    入力した数値を足し算

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
 		System.out.println(amari);
 
-		
+
 		num += 5;
 		num++;
 		System.out.println(num);
@@ -23,18 +23,42 @@ public class Main {
 		/*double d = 3;
 		int n = (int)3.0;
 		String s = "ベスト" + 3;*/
-		
+
 
 		int kokugo = 70;
 		int sansu = 75;
 
 		System.out.println((kokugo + sansu) / 2.0);
-		
-		
-		
+
+
+
 		int num1 = 3;
 		int num2 = 5;
 		System.out.println("足し算の結果は" + (num1 + num2) + "です");
-		
+
+		System.out.print("私は");
+		System.out.print("やまなか");
+		System.out.println("です");
+
+		int m = Math.max(10, 20);
+		System.out.println(m);
+		String age = "31";
+		int nAge = Integer.parseInt(age);
+		System.out.println(nAge + 1);
+
+
+		int r = new java.util.Random().nextInt(10);
+		System.out.println(r);
+
+
+		//サイコロ(1～6までのランダム)
+		int saikoro = new java.util.Random().nextInt(5);
+		System.out.print(saikoro + 1);
+
+
+
+
+
+
 	}
 }
