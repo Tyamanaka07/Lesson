2ef42f33ebffd20923a92f73738a3818d228d500
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-04-23 11:26:59 +0900

    問題回答(1~5)

---
+++ src/Loop2.java
@@ -0,0 +1,41 @@
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		//1番
+		for(int i=0; i<5; i++) {
+			System.out.print("こんにちは  ");
+		}
+		System.out.println("");
+		
+		
+		//2番
+		for(int i=1; i<=5; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println("");
+		
+		//3番
+		System.out.print("数値を入力してください：");
+		int num = new java.util.Scanner(System.in).nextInt();
+		for(int i=0; i<=num; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println("");
+		
+		//4番
+		for(int i=1; i<=10; i++) {
+			if(i%2 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println("");
+
+		//5番
+		for(int i=5; i>0; i--) {
+			System.out.print(i + " ");
+		}
+		System.out.println("");
+	}
+
+}
