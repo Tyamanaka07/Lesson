7109ed6bd4825ebc4f995a6aefc474f47b7fd51a
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-04-24 11:49:05 +0900

    Menseki回答

--- src/Menseki.java
+++ src/Menseki.java
@@ -1,26 +1,29 @@
 
 public class Menseki {
-	
+
+	//enMensekiメソッド
+//	public static void enMenseki(int hankei) {
+//		System.out.println("この円の面積は" + hankei * hankei * 3.14 + "です");
+//	}
+
+
 	//1番
 //	public static void main(String[] args) {
 //		enMenseki(10);
 //	}
-//	
-	public static void enMenseki(int hankei) {
-		System.out.println("この円の面積は" + hankei * hankei * 3.14 + "です");
-	}
+//
 
-	
+
 	//2番
-	public static void main(String[] args) {
-		System.out.println("半径1から5の円の面積を表示します");
-		for(int i=1; i<=5; i++) {
-			enMenseki(i);
-		}
-	}
-	//1番のenMensekiメソッドを使用するため省略
-	
-	
+//	public static void main(String[] args) {
+//		System.out.println("半径1から5の円の面積を表示します");
+//		for(int i=1; i<=5; i++) {
+//			enMenseki(i);
+//		}
+//	}
+
+
+
 	//3番
 //	public static void main(String[] args) {
 //		System.out.println("半径3, 8, 20の面積をそれぞれ表示します");
@@ -29,4 +32,5 @@ public class Menseki {
 //			enMenseki(h);
 //		}
 //	}
+
 }
